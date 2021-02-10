efe371b5657f48cfa9cbbf2641c186cc7fca735a
Marguerite Roth <marguerite.roth@gmail.com>
2021-02-10 08:38:00 -0500

    install gatsby source wordpress and update gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,6 +105,23 @@ module.exports = {
             },
         },
         {
+            /**
+             * First up is the WordPress source plugin that connects Gatsby
+             * to your WordPress site.
+             *
+             * visit the plugin docs to learn more
+             * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
+             *
+             */
+            resolve: `gatsby-source-wordpress`,
+            options: {
+              // the only required plugin option for WordPress is the GraphQL url.
+              url:
+                process.env.WPGRAPHQL_URL ||
+                `http://stu.local/graphql`,
+            },
+          },
+        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
