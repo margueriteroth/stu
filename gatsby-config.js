e01e7adeb6319618c97f80929c93ec9420f3c955
Marguerite Roth <marguerite.roth@gmail.com>
2021-04-18 19:54:02 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,9 +116,7 @@ module.exports = {
             resolve: `gatsby-source-wordpress`,
             options: {
               // the only required plugin option for WordPress is the GraphQL url.
-              url:
-                process.env.WPGRAPHQL_URL ||
-                `http://stu.local/graphql`,
+              url: `http://stu.local/graphql`,
             },
           },
         {
