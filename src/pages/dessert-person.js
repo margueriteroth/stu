9be095dfaad200e6fa9c7e1401378cdf795a933b
Marguerite Roth <marguerite.roth@gmail.com>
2021-05-02 19:31:16 -0400

    update dessert-person routed page

--- src/pages/dessert-person.js
+++ src/pages/dessert-person.js
@@ -2,10 +2,11 @@ import React from "react"
 import Layout from "components/Layout/Layout"
 import MaxWidth from "components/_ui/MaxWidth/MaxWidth"
 import DessertPerson from "projects/DessertPerson/DessertPerson"
+import './dessert-person.scss'
 
 const DessertPage = () => (
-    <Layout className="MoviesPage">
-        <MaxWidth className="">
+    <Layout className="DessertPage">
+        <MaxWidth size="l" className="">
             <DessertPerson />
         </MaxWidth>
     </Layout>
