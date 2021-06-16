11c0d646d32c46db21a80f955fdafb241eee074c
Marguerite Roth <marguerite.roth@gmail.com>
2021-06-16 13:37:44 -0400

    Update hooks.js

--- src/hooks/hooks.js
+++ src/hooks/hooks.js
@@ -71,3 +71,12 @@ export const useUrlParams = () => {
     const params = getUrlArgs()
     return params
 }
+
+//https://blog.logrocket.com/how-to-get-previous-props-state-with-react-hooks/
+export const usePrevious = (value) => {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
