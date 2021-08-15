4db6c02e5216528715d89ce59219294a8f40c46a
Marguerite Roth <marguerite.roth@gmail.com>
2021-08-14 22:30:58 -0400

    gatsby-node: createPages()

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,13 @@
-const path = require('path');
-const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-const BLOG_POST_FILENAME_REGEX = /([0-9]+)\-([0-9]+)\-([0-9]+)\-(.+)$/;
+const BLOG_POST_FILENAME_REGEX = /([0-9]+)\-([0-9]+)\-([0-9]+)\-(.+)$/
 // For post folders with [DATE]-post-title-for-url format,
 // separate name and date to isolate when making full canonical url.
 // This is nice because I can organize my post folders by date,
 // but not have that bleed through to the url.
 exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions;
+    const { createNodeField } = actions
 
     if (node.internal.type === `Mdx`) {
         if (node.internal.type === `Mdx`) {
@@ -26,71 +26,72 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
                 createNodeField({
                     name: `slug`,
                     node,
-                    value: `/${filename}`,
+                    value: `/${filename}`
                 })
 
                 createNodeField({
                     name: `date`,
                     node,
-                    value: date.toJSON(),
+                    value: date.toJSON()
                 })
             } else {
                 createNodeField({
                     name: `slug`,
                     node,
-                    value: slug,
+                    value: slug
                 })
             }
         }
     }
 }
 
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
 
-// Sample query and function for dynamically generating pages from MDX posts.
-// Make sure the correct node module is installed.
-//
-// exports.createPages = async ({ graphql, actions }) => {
-//     const { createPage } = actions
-//
-//
-//     const blogPost = path.resolve(`src/templates/Post/Post.jsx`);
-//     const blogPosts = await graphql(`
-//         query {
-//             allMdx(filter: {frontmatter: {type: {ne: "internal"}}, fileAbsolutePath: {regex: "/blog/"}}, sort: { fields: [frontmatter___date], order: DESC }) {
-//                 edges {
-//                     node {
-//                         fields {
-//                             slug
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//   `)
+    const blogPost = path.resolve(`src/templates/Post/Post.jsx`)
 
-//     if (blogPosts.errors) {
-//         throw blogPosts.errors
-//     }
+    const blogPosts = await graphql(`
+        query {
+            allMdx(
+                filter: { fileAbsolutePath: { regex: "/blog/" } }
+                sort: { fields: [frontmatter___date], order: DESC }
+            ) {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    `)
 
-//     // Create work post pages.
-//     const posts = blogPosts.data.allMdx.edges
+    if (blogPosts.errors) {
+        throw blogPosts.errors
+    }
 
-//     posts.forEach((post, index) => {
-//         const previous = index === posts.length - 1 ? null : posts[index + 1].node
-//         const next = index === 0 ? null : posts[index - 1].node
+    // Create work post pages.
+    const posts = blogPosts.data.allMdx.edges
 
-//         createPage({
-//             path: `/blog${post.node.fields.slug}`,
-//             component: blogPost,
-//             context: {
-//                 slug: post.node.fields.slug,
-//                 previous,
-//                 next,
-//             },
-//         })
-//     })
-//}
+    console.log(posts)
 
+    posts.forEach((post, index) => {
+        const previous =
+            index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+
+        createPage({
+            path: `/blog${post.node.fields.slug}`,
+            component: blogPost,
+            context: {
+                slug: post.node.fields.slug,
+                previous,
+                next
+            }
+        })
+    })
+}
 
 // Because we need CSV for d3!!
 exports.onCreateWebpackConfig = ({
@@ -98,26 +99,26 @@ exports.onCreateWebpackConfig = ({
     rules,
     loaders,
     plugins,
-    actions,
+    actions
 }) => {
     actions.setWebpackConfig({
         module: {
             rules: [
                 {
                     test: /\.csv$/,
-                    loader: 'csv-loader',
+                    loader: "csv-loader",
                     options: {
                         dynamicTyping: true,
                         header: true,
                         skipEmptyLines: true
                     }
-                },
-            ],
+                }
+            ]
         },
         plugins: [
             plugins.define({
-                __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
-            }),
-        ],
+                __DEVELOPMENT__: stage === `develop` || stage === `develop-html`
+            })
+        ]
     })
 }
