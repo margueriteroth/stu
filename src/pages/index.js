8fb2a4eb5fed6a99a8d2f7a1d3bff93ea8d415cd
Marguerite Roth <marguerite.roth@gmail.com>
2021-08-16 19:52:26 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,26 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Feed from "components/Feed/Feed"
+import Intro from "components/Intro/Intro"
+import Footer from "components/Footer/Footer"
 import Layout from "components/Layout/Layout"
 import MaxWidth from "components/_ui/MaxWidth/MaxWidth"
 import MoviesContainer from "components/Movies/MoviesContainer"
 import "./index.scss"
 
-const HomePage = () => (
-    <StaticQuery
-        query={FEED_QUERY}
-        render={data => (
-            <Layout>
-                <MaxWidth size="l" className="Home">
-                    <div className="Home__greeting">
-                        Hello! I'm Marguerite — a designer and software
-                        engineer.
-                    </div>
-                    <Feed />
-                </MaxWidth>
-            </Layout>
-        )}
-    />
-)
+const HomePage = ({ data }) => {
+    console.log(data)
+    return (
+        <Layout>
+            <MaxWidth size="m" className="Home">
+                {/* <Intro /> */}
+                {/* <div className="Home__greeting">
+                    Hello! I'm Marguerite — a designer and software engineer.
+                </div> */}
+                <Feed />
+            </MaxWidth>
+        </Layout>
+    )
+}
 
 export default HomePage
-
-const FEED_QUERY = graphql`
-    query {
-        allMdx(
-            filter: {frontmatter: {type: {ne: "internal"}}, fileAbsolutePath: {regex: "/blog/"}}
-            sort: { fields: [frontmatter___date], order: DESC }
-        ) {
-            edges {
-                node {
-                    id
-                    fields {
-                        slug
-                    }
-                    frontmatter {
-                        title
-                        date(formatString: "MMMM D, YYYY")
-                    }
-                }
-            }
-        }
-    }
-`
